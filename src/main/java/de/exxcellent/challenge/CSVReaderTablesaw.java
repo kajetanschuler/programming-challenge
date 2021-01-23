2ebc5064fb7dfb666badce242f46a8b864a9b73d
Kajetan Schuler <kajetan.schuler@gmail.com>
2021-01-23 15:23:07 +0100

    Implement reading of .csv file

--- src/main/java/de/exxcellent/challenge/CSVReaderTablesaw.java
+++ src/main/java/de/exxcellent/challenge/CSVReaderTablesaw.java
@@ -1,21 +1,22 @@
 package de.exxcellent.challenge;
 
 import tech.tablesaw.api.Table;
-
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 
+/**
+ * Class to handle CSV files as input
+ */
 public class CSVReaderTablesaw implements FileReader {
-    private final URL filePath;
 
-    public CSVReaderTablesaw(URL filePath) {
-        this.filePath = filePath;
-    }
-
+    /**
+     * Reads the specified .csv file and returns Table object
+     * @param filePath URL to the file that should be read
+     * @return Returns a Table Object for further processing
+     * @throws IOException Throws Excpetion if file cannot be read or accessed
+     */
     @Override
-    public Table readFile() throws FileNotFoundException, IOException {
-        Table dataTable = Table.read().csv(filePath);
-        return dataTable;
+    public Table readFile(URL filePath) throws IOException {
+        return Table.read().csv(filePath);
     }
 }
