ef9d7f81d0746eaf617767b0b4ab6d875ba90673
Kajetan Schuler <kajetan.schuler@gmail.com>
2021-01-23 16:28:31 +0100

    Add test case for minimalSpread calculation

--- src/test/java/de/exxcellent/challenge/AppTest.java
+++ src/test/java/de/exxcellent/challenge/AppTest.java
@@ -2,12 +2,16 @@ package de.exxcellent.challenge;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import tech.tablesaw.api.Table;
+import java.io.IOException;
+import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Example JUnit 5 test case.
- * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
+ * @author Benjamin Schmid <benjamin.schmid@exxcellent.de> (Challenge Author)
+ * @author Kajetan Schuler <kajetan.schuler@gmail.com> (Challenge Solution)
  */
 class AppTest {
 
@@ -26,6 +30,24 @@ class AppTest {
     @Test
     void runFootball() {
         App.main("--football", "football.csv");
+    }
+
+    @Test
+    public void minimumSpreadTest() {
+        Table testData = null;
+        URL weatherDataPath = App.class.getResource("test.csv");
+        CSVReaderTablesaw csvReader = new CSVReaderTablesaw();
+
+        try {
+            testData = csvReader.readFile(weatherDataPath);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        final DataAnalyzer analyzer = new DataAnalyzer(testData, "min", "max", "label");
+
+        final String successLabel = analyzer.calculateMinimalSpread();
+        assertEquals("yes", successLabel, "record not correct");
     }
 
 }
